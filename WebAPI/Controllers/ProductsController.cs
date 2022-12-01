2b323178e0d57543f29c15b906e9fdadff2e0af5
Alparslan <alparslanaydgn02@gmail.com>
2022-12-02 00:37:22 +0300

    Projeye API başlangıcı eklendi.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]//https://localhost:44367/api/products bu linke gidersek 
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()//burası gelicek
+        {
+            return new List<Product>
+            {
+                new Product{ProductID=1 , ProductName="elma"},
+                new Product{ProductID=2 , ProductName="armut"}
+            };
+        }
+    }
+}
